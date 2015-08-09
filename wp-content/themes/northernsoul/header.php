130458d12d445ca476a42a088a7bf4fa0ff77f6e
Charlie Anstey <charlie.anstey@degree53.com>
2015-08-09 12:48:25 +0100

    Updated to style matching design

--- wp-content/themes/northernsoul/header.php
+++ wp-content/themes/northernsoul/header.php
@@ -9,6 +9,20 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<!-- CSS -->
 	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/css/main.min.css">
+	<!-- JS -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script>
+      function initialize() {
+        var mapCanvas = document.getElementById('map-canvas');
+        var mapOptions = {
+          center: new google.maps.LatLng(53.4830218, -2.237841),
+          zoom: 16,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
 	<?php wp_head(); ?>
 </head>
 <body>
--- wp-content/themes/northernsoul/index.php
+++ wp-content/themes/northernsoul/index.php
@@ -3,28 +3,35 @@
 	<!-- <div style="height: 50px; background: #f00; width: 50%;"></div> -->
 	<section class="box board big-box">
 		<img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.svg">
-		<img class="twitter" src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.svg">
+		<img class="twitter-bird" src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.svg">
 		<div class="tagline">
 			<h1 class="tagline-gourmet">Gourmet</h1>
 			<h1 class="tagline-grilled">Grilled Cheese</h1>
 			<h1 class="tagline-soups">Soups 'n' Shakes</h1> </div>
 	</section>
 	<section class="box black big-box">
-		<h1>You have arrived</h1>
-		<h2>Welcome to <br>Manchester's premier gourmet grilled cheese <br>pop up. Banging out killer eats and rare beats</h2>
+		<h1>You have<br />arrived</h1>
+		<h2>Welcome to Manchester's premier gourmet grilled cheese pop up. Banging out killer eats and rare beats</h2>
 		<h4>#KeepTheFaith</h4>
 	</section>
 	<section class="box white small-box">
 		<h1 class="dot">Location</h1>
+		<div id="map-canvas" style="width:86%;padding-bottom:55%;margin:0 auto;border:5px solid #000;"></div>
 	</section>
 	<section class="box menu small-box">
 		<h1 class="dot">Menu</h1>
 	</section>
 	<section class="box yellow small-box">
 		<h1 class="dot">Social</h1>
+		<div class="tweets">
+			<a class="twitter-timeline" href="https://twitter.com/search?q=from%3Anorthernsoulmcr%20-RT" data-widget-id="618198142215921664" data-chrome="noheader nofooter noborders noscrollbar transparent" data-tweet-limit="1">Tweets by @NorthernSoulMcr</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<img class="twitter-bird" src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.svg">
 	</section>
 	<section class="box black small-box">
-		<h1>Are you a big fan of grilled cheese?</h1>
+		<h1>Are you a<br />big fan of<br />grilled<br />cheese?</h1>
+		<div class="tap-button"><h4 style="color: black; text-decoration: underline; font-size: 26px; margin: 0;">TAP</h4></div>
 	</section>
 	<section class="box board small-box"></section>
 
